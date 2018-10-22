14aafca261a801e8dc1d8f84728b8945e1fe2df9
Andrew Bernier <a6bernier@gmail.com>
2018-10-22 18:34:15 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,20 +1,20 @@
 class User
 
-  def first_name=(first_name)
-    @first_name = first_name
-  end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name=(last_name)
-    @last_name = last_name
-  end
-
-  def last_name
-    @last_name
-  end
+  # def first_name=(first_name)
+  #   @first_name = first_name
+  # end
+  #
+  # def first_name
+  #   @first_name
+  # end
+  #
+  # def last_name=(last_name)
+  #   @last_name = last_name
+  # end
+  #
+  # def last_name
+  #   @last_name
+  # end
 
   attr_accessor :first_name, :last_name
 
